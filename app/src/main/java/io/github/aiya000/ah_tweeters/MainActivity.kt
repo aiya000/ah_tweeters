90efa8c95aed597293bef19e4a494d4d6af691c2
aiya000 <aiya000.develop@gmail.com>
2017-03-18 19:30:56 +0900

    Convert to Kotlin project

---
+++ app/src/main/java/io/github/aiya000/ah_tweeters/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.aiya000.ah_tweeters
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
