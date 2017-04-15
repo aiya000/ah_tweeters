0f021dfe5ba616197656cc9c15891cefa2b8720f
aiya000 <aiya000.develop@gmail.com>
2017-04-15 16:34:28 +0900

    Add a menu to MainActivity

--- app/src/main/java/io/github/aiya000/ah_tweeters/MainActivity.kt
+++ app/src/main/java/io/github/aiya000/ah_tweeters/MainActivity.kt
@@ -1,12 +1,18 @@
 package io.github.aiya000.ah_tweeters
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.*
+import android.view.Menu
+
 
 class MainActivity : AppCompatActivity() {
+	override fun onCreateOptionsMenu(menu: Menu): Boolean {
+		getMenuInflater().inflate(R.menu.activity_main, menu)
+		return true
+	}
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
 }
